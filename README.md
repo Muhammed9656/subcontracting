f53cab3c6115c4d51cc6b61c113d015fda007d4e
muhammed <muhammed@muhammed-HP-Pavilion-Gaming-Laptop-15-ec2xxx>
2024-05-05 19:18:48 +0530

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Subcontracting
+
+subcontracting
+
+#### License
+
+MIT
---
+++ subcontracting/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Subcontracting",
+			"type": "module",
+			"label": _("Subcontracting")
+		}
+	]
