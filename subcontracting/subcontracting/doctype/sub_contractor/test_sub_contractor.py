c3a1826066c23acacdb5ab60174f09718489b39a
Muhammed ibnu Arif <muhammedibnurif3@gmail.com>
2024-05-08 22:23:41 +0530

    test commit

---
+++ subcontracting/subcontracting/doctype/sub_contractor/test_sub_contractor.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Muhammed ibnu arif and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSubContractor(FrappeTestCase):
+	pass
